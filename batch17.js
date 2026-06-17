37377c307d3dac4ed6ec3d99f68594d1073ddab5
Deepak <deepak63474774@gmail.com>
2026-06-17 11:15:49 +0530

    JS

---
+++ batch17.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple HTML with JS</title>
+</head>
+<body>
+
+    <h1>Hello World</h1>
+    <p id="message">Click the button</p>
+
+    <button onclick="showMessage()">Click Me</button>
+
+    <script src="batch417.js"></script>
+
+</body>
+</html>
---
+++ batch417.js
@@ -0,0 +1,24 @@
+function example(){
+    console.log("Hi")
+}
+
+const example1 = ()=>{
+    console.log("Hello")
+}
+
+console.log("A")
+setTimeout(()=>{
+    console.log("B")
+},0)
+console.log("C")
+
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        console.log(1);
+    },2)
+    console.log("3")
+
+})
+
+promise.then(res=>{console.log(res)})
+.catch(err=>console.log(err))
